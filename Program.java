c6d8f9c10f89f92f8663b7fb7f6944f29d56e265
Philipp-Lange <philippalexanderlange@gmail.com>
2023-07-30 20:58:27 +0100

    Got rid of text user interface as it was no longer needed.

--- Program.java
+++ Program.java
@@ -2,11 +2,8 @@ public class Program {
 
     public static void main(String[] args) {
         Game game = new Game();
-        UserInterface ui = new UserInterface(game);
 
-        ui.start();
         new GameFrame(game);
-
     }
 
 }
